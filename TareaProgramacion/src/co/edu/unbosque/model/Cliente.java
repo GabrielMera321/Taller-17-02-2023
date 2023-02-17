953161687323de5ec98e921dc49b2c00e37cd34b
GabrielMera321 <105569564+GabrielMera321@users.noreply.github.com>
2023-02-17 07:28:50 -0500

    Add files via upload

---
+++ TareaProgramacion/src/co/edu/unbosque/model/Cliente.java
@@ -0,0 +1,31 @@
+package co.edu.unbosque.model;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+public class Cliente {
+    private Socket socket;
+    private BufferedReader reader;
+
+    public Cliente(Socket socket) {
+        this.socket = socket;
+        try {
+            this.reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void receiveAndPrint() {
+        String message;
+        try {
+            while ((message = reader.readLine()) != null) {
+                System.out.println(message);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
